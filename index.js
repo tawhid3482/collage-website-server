95c1ffd3e6338b8a9f93e975405f3ff08ad32c3d
saikat <tawhidulislam3482@gmail.com>
2024-01-09 15:57:40 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s64u1mi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,16 +17,34 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const departmentCollection = client
+      .db("collageDb")
+      .collection("department");
+      const eventsCollection = client.db('collageDb').collection('events')
+
+    app.get("/department", async (req, res) => {
+      const result = await departmentCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/events",async(req,res)=>{
+      const result = await eventsCollection.find().toArray()
+      res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -35,11 +52,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/',(req,res)=>{
-    res.send('collage is running')
-})
-app.listen(port, ()=>{
-    console.log(`collage is running ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("collage is running");
+});
+app.listen(port, () => {
+  console.log(`collage is running ${port}`);
+});
