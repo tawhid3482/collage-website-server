87821dcea80aeddd97e3a1798357b6a9cc802f97
saikat <tawhidulislam3482@gmail.com>
2024-02-06 18:31:53 +0600

    students action delete working

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // department
     app.get("/department", async (req, res) => {
       const result = await departmentCollection.find().toArray();
